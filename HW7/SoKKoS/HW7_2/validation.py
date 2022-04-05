a78d1014563e034790af45df082de1693042df92
7SoKKoS7 <97355065+7SoKKoS7@users.noreply.github.com>
2022-04-05 15:46:44 +0300

    add CW_7_1

--- HW7/SoKKoS/HW7_2/validation.py
+++ HW7/SoKKoS/HW7_2/validation.py
@@ -2,7 +2,7 @@ import re
 
 def valid_pass(password_user):
     print(password_user)
-    if re.match('((?=.*[a-z])(?=.*[$#@])(?=.*[A-Z]).{6,16})', password_user) and len(password_user) <= 16:
+    if re.match('((?=.*[a-z])(?=.*[$#@])(?=.*[A-Z]).{6})', password_user) and len(password_user) <= 16:
         return False
     else:
         return True

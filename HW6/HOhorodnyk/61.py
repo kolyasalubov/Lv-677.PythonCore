194fa5ed74376ecf2a7ec86241c09c4fe6895393
Halyna Ohorodnyk <g.ohorodnyk@gmail.com>
2022-04-06 20:51:44 +0300

    added HW 61

---
+++ HW6/HOhorodnyk/61.py
@@ -0,0 +1,9 @@
+number_1 = 34
+number_2 = 8
+
+def the_lagest_number (number_1, number_2):
+    ''' fanction return the lagest number of two number'''
+    number = max(number_1,number_2)
+    return number
+
+print(f'{the_lagest_number(number_1,number_2)} is the lagest')

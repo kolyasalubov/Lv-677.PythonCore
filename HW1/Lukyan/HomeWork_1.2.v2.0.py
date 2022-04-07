26eecc135077ebf2fefe2c462e62c2c563f7a5ad
Lukyan Chornobai <elertime@gmail.com>
2022-04-07 06:14:44 +0300

    aded HW1 HW2 H3

---
+++ HW1/Lukyan/HomeWork_1.2.v2.0.py
@@ -0,0 +1,4 @@
+function=input("Choose funcion(+,-,*,/,**)\n")
+a=input("Write the a\n")
+b=input("Write the b\n")
+print(a,function,b,'=',eval(a+function+b))
---
+++ HW3/Lukyan/HW3.3.py
@@ -0,0 +1,5 @@
+first_variable=input("Write first variable\n")
+second_variable=input("Write second variable\n")
+first_variable,second_variable=second_variable,first_variable
+print("First variable",first_variable)
+print("Second variable",second_variable)

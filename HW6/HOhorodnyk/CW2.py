9b6996508e1d6a1981f75d3e12288bd98e203fc3
Halyna Ohorodnyk <g.ohorodnyk@gmail.com>
2022-04-08 20:27:39 +0300

    added CW1, CW2

---
+++ HW6/HOhorodnyk/CW2.py
@@ -0,0 +1,10 @@
+def filter_words(st):
+    """
+    Write a function taking in a string like 
+    WOW this is REALLY amazing 
+    and returning Wow this is really amazing. 
+    """
+    return st.capitalize()
+    
+    
+    
---
+++ HW6/HOhorodnyk/test.py
@@ -0,0 +1,10 @@
+# def function_name(parameters):
+#     ''' Docstring'''
+#     statement(s)
+#     return
+st = "hello WORLD?"
+
+def filter_words(st):
+    return st.capitalize()
+
+print(filter_words)

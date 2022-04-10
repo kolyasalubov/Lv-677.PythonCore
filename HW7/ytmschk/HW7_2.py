634bd06b7262eb65ef7dda96468aa3fec1715448
tymoschuk.mail@gmail.com <macbookpro@yt.local>
2022-04-10 17:38:49 +0300

    Katas added

--- HW7/ytmschk/HW7_2.py
+++ HW7/ytmschk/HW7_2.py
@@ -1,20 +1,12 @@
-# import re
-# pattern = "^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$"
-# password = input("Enter string to test: ")
-# result = re.findall(pattern, password)
-# if (result):
-#     print("Valid password")
-# else:
-#     print("Password not valid")
+import re
 
-# import re
-# password = input("Enter your password: ")
-# if re.fullmatch(r'[A-Za-z0-9$#@]{6,16}', password):
-#     print ("Welcome! Your password is correct")
-# else:
-print ('Please try again.\n',
-    'At least 1 letter between [a-z] and 1 letter between [A-Z].\n',
-    'At least 1 number between [0-9].\n',
-    'At least 1 character from [$#@].\n',
-    'Minimum length 6 characters.\n',
-    'Maximum length 16 characters.')
+password = input("Enter your password: ")
+if re.fullmatch(r'[A-Za-z0-9$#@]{6,16}', password):
+    print ("Welcome! Your password is correct")
+else:
+    print ('Please try again. Your password should contain: \n',
+        '1. At least 1 letter between [a-z] and 1 letter between [A-Z].\n',
+        '2. At least 1 number between [0-9].\n',
+        '3. At least 1 character from [$#@].\n',
+        '4. Minimum length 6 characters.\n',
+        '5. Maximum length 16 characters.')

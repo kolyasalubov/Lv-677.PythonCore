6a621cbd8b670fc03bff1c0bda667dcfa3a56b71
Yuliia Kutsyk <yulia.kutsyk@mail.com>
2022-04-12 22:29:07 +0300

    Add HW1/YuliiaKutsyk

---
+++ HW1/YuliiaKutsyk/task_1_2.py
@@ -0,0 +1,8 @@
+a = int(input("Input number a:\n"))
+b = int(input("Input number b:\n"))
+
+print(f"a + b = {a+b}")
+print(f"a - b = {a-b}")
+print(f"a * b = {a*b}")
+print(f"a / b = {a/b}")
+print(f"a ** b = {a**b}")

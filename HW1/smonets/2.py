0794363b18f721fb2215fd0f3f47433f7980efc9
Yuliia Kutsyk <yulia.kutsyk@mail.com>
2022-04-13 22:03:48 +0300

    reload correct files to HW5. delete directories

--- HW1/smonets/2.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Введіть змінну a: "))
-b = int(input("Введіть змінну b: "))
-
-print("a + b =",a + b)
-print("a - b =",a - b)
-print("a * b =",a * b)
-print("a / b =",a / b)
-print("a ** b =",a ** b)

637cfa5db16918a7a006d7037f67a5613d38c84a
Halyna Ohorodnyk <g.ohorodnyk@gmail.com>
2022-04-13 20:56:53 +0300

    added CW 7.1 & 7.7

--- HW6/HOhorodnyk/test.py
+++ HW6/HOhorodnyk/test.py
@@ -2,9 +2,25 @@
 #     ''' Docstring'''
 #     statement(s)
 #     return
-st = "hello WORLD?"
+# st = "hello WORLD?"
 
-def filter_words(st):
-    return st.capitalize()
+# def filter_words(st):
+#     st_cap = st.capitalize()
+#     return
 
-print(filter_words)
+# print(filter_words)
+
+
+# a=90
+# print(dir())
+# print(__name__)
+
+
+# import math
+# print (math.sqrt(9))
+
+import time
+import random
+
+print(time.time())
+print (random.random())

7e553c5e477c532011c1f896e2edbe74e5859213
Zoriana Matsko <zoriana.matsko@gmail.com>
2022-05-15 19:32:56 +0200

    Added homework Git

---
+++ HW3/Zoriana007/Homework task 2 (3 lecture).py
@@ -0,0 +1,26 @@
+number = input("Please input 4 digit number: ")
+
+number_list = list(number) 
+
+multiplication_number = int(number_list[0]) * int(number_list[1]) * int(number_list[2]) * int(number_list[3])
+
+number_list_reversed = number_list.reverse()
+number_reversed = "".join(number_list)
+
+#Сортировка та поєднання списку
+number_list_sorted = number_list
+number_list_sorted.sort()
+number_sorted = "".join(number_list_sorted)
+
+#Вивід
+print("Добуток цифр цього числа", multiplication_number,"\nЧисло в реверсному порядку", number_reversed,"\nПосортувати цифри, що входять в дане число: ", number_sorted)
+
+
+
+
+
+
+# -Задано чотирицифрове натуральне число.
+# - Знайти добуток цифр цього числа.
+# - Записати число в реверсному порядку.
+# - Посортувати цифри, що входять в дане число

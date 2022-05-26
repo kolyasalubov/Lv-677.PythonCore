f45296311b35d785bdf69010677ace645ecd7a1f
Mykyta Marchukov <digidod43@gmail.com>
2022-05-26 19:35:13 +0300

    added cw4

---
+++ CW_all/Mykyta_Marchukov/CW_4_1-4.py
@@ -0,0 +1,42 @@
+########## 1
+#Regular Ball Super Ball
+# class Ball(object):
+#     def __init__(self, ball_type="regular"):
+#         self.ball_type = ball_type
+
+
+########## 2
+#Color Ghost
+# import random
+#
+# class Ghost(object):
+#     def __init__(self):
+#         self.color = random.choice(["white", "yellow", "purple", "red"])
+
+
+########## 3
+#Basic subclasses - Adam and Eve
+# class Human:
+#     def __init__(self):
+#         pass
+
+# class Man(Human):
+#     def __init__(self):
+#         super().__init__()
+
+# class Woman(Human):
+#     def __init__(self):
+#         super().__init__()
+
+# def God():
+#     m = Man()
+#     w = Woman()
+#     result = [m,w]
+#     return result
+
+
+########## 4
+#Classy Classes
+# class Person:
+#     def __init__(self, name, age):
+#         self.info = f"{name}s age is {age}"
